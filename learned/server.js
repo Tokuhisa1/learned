2873ec1e3369c5c1cf1ac8aad562afd959860c21
J Tokuhisa <TokuhisaWinston@Outlook.com>
2017-10-11 17:19:04 -0400

    creates express server for application

---
+++ learned/server.js
@@ -0,0 +1,14 @@
+// Imports Express from dependencies
+const express = require('express');
+
+// Initializes the application
+const app = express();
+
+// Sets the port, if not defined
+const port = process.env.PORT || 3000;
+
+
+// Tells the application which port to monitor
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
