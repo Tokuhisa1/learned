1224ea9e8975c267a298a156c701213a75246414
J Tokuhisa <TokuhisaWinston@Outlook.com>
2017-10-11 17:35:14 -0400

    installs nodemon as a dependency and generates feedback in browser

--- learned/server.js
+++ learned/server.js
@@ -12,3 +12,8 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+// Creates an index route
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
