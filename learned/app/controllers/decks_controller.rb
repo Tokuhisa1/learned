54d0631615ce0bcc0b7cf9a3851d5f31866d911e
J Tokuhisa <TokuhisaWinston@Outlook.com>
2017-10-26 13:40:53 -0400

    removes token before pulling in changes from react branch

--- learned/app/controllers/decks_controller.rb
+++ learned/app/controllers/decks_controller.rb
@@ -5,4 +5,17 @@ class DecksController < ApplicationController
     decks = HTTParty.get('http://memjogger.com/api/cardset?token=')
     render json: { message: "ok", deck_data: decks }
   end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
