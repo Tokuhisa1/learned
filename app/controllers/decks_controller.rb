89d1522fa8662b9dbb4c77237af8ed5a02918ec1
J Tokuhisa <TokuhisaWinston@Outlook.com>
2017-11-06 10:50:43 -0500

    changes proxy address and token for deployment

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -2,29 +2,29 @@ require 'httparty'
 
 class DecksController < ApplicationController
   def index
-    decks = HTTParty.get('http://memjogger.com/api/cardset?token=6dce93485a8fb619c6536793db63d60c')
+    decks = HTTParty.get('http://memjogger.com/api/cardset?token=23eaef38e53e228e82c0095a4f5beff8')
     render json: { message: "ok", decks_data: decks }
   end
 
   def create
-    new_deck = HTTParty.post('http://memjogger.com/api/cardset?token=6dce93485a8fb619c6536793db63d60c',
+    new_deck = HTTParty.post('http://memjogger.com/api/cardset?token=23eaef38e53e228e82c0095a4f5beff8',
       :body => { :name => params[:name] }.to_json)
     render json: { message: "ok", new_data: new_deck }
   end
 
   def show
-    deck = HTTParty.get("http://memjogger.com/api/cardset/#{params[:id]}?token=6dce93485a8fb619c6536793db63d60c")
-    cards = HTTParty.get("http://memjogger.com/api/cardset/#{params[:id]}/cards?token=6dce93485a8fb619c6536793db63d60c")
+    deck = HTTParty.get("http://memjogger.com/api/cardset/#{params[:id]}?token=23eaef38e53e228e82c0095a4f5beff8")
+    cards = HTTParty.get("http://memjogger.com/api/cardset/#{params[:id]}/cards?token=23eaef38e53e228e82c0095a4f5beff8")
     render json: { message: "ok", deck_data: [deck, cards]}
   end
 
   def update
-    revision = HTTParty.put("http://memjogger.com/api/cardset/#{params[:id]}?token=6dce93485a8fb619c6536793db63d60c",
+    revision = HTTParty.put("http://memjogger.com/api/cardset/#{params[:id]}?token=23eaef38e53e228e82c0095a4f5beff8",
       :body => { :name => params[:name] }.to_json)
     render json: { message: "ok", new_data: revision }
   end
 
   def destroy
-    HTTParty.delete("http://memjogger.com/api/cardset/#{params[:id]}?token=6dce93485a8fb619c6536793db63d60c")
+    HTTParty.delete("http://memjogger.com/api/cardset/#{params[:id]}?token=23eaef38e53e228e82c0095a4f5beff8")
   end
 end
