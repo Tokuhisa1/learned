6185bcce28b31280fa0f90488344ec1443b47182
J Tokuhisa <TokuhisaWinston@Outlook.com>
2017-11-06 10:54:38 -0500

    changes token in cards controller for production

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,7 +8,7 @@ class CardsController < ApplicationController
   end
 
   def show
-    card = HTTParty.get("http://memjogger.com/api/card/#{params[:id]}?token=6dce93485a8fb619c6536793db63d60c")
+    card = HTTParty.get("http://memjogger.com/api/card/#{params[:id]}?token=23eaef38e53e228e82c0095a4f5beff8")
     render json: { message: "ok", card_data: card }
   end
 
